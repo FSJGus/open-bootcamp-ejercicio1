7ff844986fe9b239b44d973b7264a94e96fb94be
Gustavo Lima <limasud@gmail.com>
2022-05-27 20:42:46 -0400

    First Commit

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+// **Mensaje de bienvenida**
+console.log("**El script principal llamado main.js funciona.**");
+
+// **Desarrollo de la lógica**
+
+// **Creación de validaciones**
+
+// **Consumo de API**
+
+// Mensaje de despedida
+console.log("Adiós...");
+
+
+
